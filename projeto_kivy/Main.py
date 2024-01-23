ab92d7f672ff45f51324ea10d2258d7661c56d5a
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-01-23 08:54:39 -0300

    Exibindo uma imagem na janela

--- projeto_kivy/Main.py
+++ projeto_kivy/Main.py
@@ -1,12 +1,12 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.image import Image, AsyncImage
 
 class MainApp(App):
     def build(self):
-        label= Label(text='Fala galera!',
-                     size_hint=(.5,.5),
-                     pos_hint={'center_x': .5, 'center_y': .5})
-        return label
+        img= AsyncImage(source='https://supermariorun.com/assets/img/stage/mario03.png',
+                        size_hint=(1, .5),
+                        pos_hint={'center_x':.5, 'center_y':.5})
+        return img
     
 if __name__=='__main__':
     app= MainApp()
