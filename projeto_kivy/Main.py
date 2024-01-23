d03dc6a76711446f1c22a890253d89a1613b86fd
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-01-23 09:01:15 -0300

    Criando layouts com texto e botões na tela

--- projeto_kivy/Main.py
+++ projeto_kivy/Main.py
@@ -1,13 +1,23 @@
+import random
 from kivy.app import App
-from kivy.uix.image import Image, AsyncImage
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-class MainApp(App):
+red= [1,0,0,1]
+green= [0,1,0,1]
+blue= [0,0,1,1]
+purple= [1,0,1,1]
+
+class HBoxLayoutExample(App):
     def build(self):
-        img= AsyncImage(source='https://supermariorun.com/assets/img/stage/mario03.png',
-                        size_hint=(1, .5),
-                        pos_hint={'center_x':.5, 'center_y':.5})
-        return img
-    
-if __name__=='__main__':
-    app= MainApp()
-    app.run()
+        layout= BoxLayout(padding=10)
+        colors= [red, green, blue, purple]
+        
+        for i in range(5):
+            btn= Button(text=f'Este é o botão #{i+1}',
+                        background_color=random.choice(colors))
+            layout.add_widget(btn)
+        return layout
+if __name__== "__main__":
+    app= HBoxLayoutExample()
+    app.run() 
